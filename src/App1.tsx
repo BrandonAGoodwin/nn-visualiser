42608eec647f679c279a58a4a3b0ecc6aba6c9db
Brandon Goodwin-Adeleke <bxg796@cs.bham.ac.uk>
2020-11-02 18:06:20 +0000

    Created first D3 object fully rendered (without external data input)

---
+++ src/App1.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './App.css';
+
+type AppProps = {
+    
+}
+
+export default class App extends React.Component<AppProps, {}> {
+
+}
---
+++ src/ScatterPlot.ts
@@ -0,0 +1,14 @@
+import * as d3 from "d3";
+
+// export class ScatterPlot {
+//     //private xScale : number;
+//     //private yScale : number;
+
+//     constructor(
+//         width: number, xDomain: [number, number], yDomain: [number, number], container
+//     ) {
+//         let hight = width;
+//         let padding = 20; // Arbitrary
+
+//     }
+// }
