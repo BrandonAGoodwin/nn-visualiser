36c2aecca3759187b677c662c97cc2f7fdd380ca
Brandon Goodwin-Adeleke <bxg796@cs.bham.ac.uk>
2021-01-21 17:01:35 +0000

    Added info to dataset panel

--- src/components/InfoPanels/DatasetInfoPanel.tsx
+++ src/components/InfoPanels/DatasetInfoPanel.tsx
@@ -5,8 +5,13 @@ function DatasetInfoPanel(props: NNConfig) {
 
     return (
         <div>
-            <h1>Datasets Panel</h1>
-            <p></p>
+            <h1>Datasets</h1>
+            <p>The are the data sets you can use to train and test your network.</p>
+            <h2>Gaussian</h2>
+            <p>This data set is comprised of 2 2-Dimensional Gaussian (or Normal) distributed sets of points, one for each class.<br/>
+            <a href="https://en.wikipedia.org/wiki/Normal_distribution" target="_blank">Read more here (Wiki)</a></p>
+            <h2>XOR</h2>
+            <p>This data set represents an XOR distributed set of points which cannot be classified using a straight line.</p>
         </div>
     );
 }
