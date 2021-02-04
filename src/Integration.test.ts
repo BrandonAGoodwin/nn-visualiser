91ac915da47f6d4c5f55269024cf66fe4b383e73
Brandon Goodwin-Adeleke <bxg796@cs.bham.ac.uk>
2021-02-04 16:04:13 +0000

    Updated Integration tests with new inputs format.

--- src/Integration.test.ts
+++ src/Integration.test.ts
@@ -10,34 +10,42 @@ describe("Gradient Decent", () => {
     let learningRate = 0.03;
     let noSteps = 300;
     let defaultNetworkShape = [2, 1];
-    let defaultInputIds = ["x", "y"];
+    let defaultInputs = {
+        "x": true,
+        "y": true,
+        "xSquared": false,
+        "ySquared": false,
+        "xTimesY": false,
+        "sinX": false,
+        "sinY": false
+    };
     let defaultOutputActivation = nn.Activations.TANH;
     let defaultBatchSize = 10;
 
 
     it("Sigmoid", () => {
-        network = nn.generateNetwork(defaultNetworkShape, nn.Activations.SIGMOID, defaultOutputActivation, defaultInputIds);
+        network = nn.generateNetwork(defaultNetworkShape, nn.Activations.SIGMOID, defaultOutputActivation, defaultInputs);
         for (let i = 0; i < noSteps; i++) {
-            vis.step(network, trainingData, learningRate, defaultInputIds, defaultBatchSize);
+            vis.step(network, trainingData, learningRate, defaultInputs, defaultBatchSize);
         }
 
         for (let i = 0; i < trainingData.length; i++) {
             let datapoint = trainingData[i];
-            let prediction = nn.forwardPropagate(network, vis.constructInputs(datapoint.x1, datapoint.x2, defaultInputIds)) > 0 ? 1 : -1;
+            let prediction = nn.forwardPropagate(network, vis.constructInputs(datapoint.x1, datapoint.x2, defaultInputs)) > 0 ? 1 : -1;
             expect(prediction).toEqual(datapoint.y);
         }
 
     })
 
     it("ReLU", () => {
-        network = nn.generateNetwork(defaultNetworkShape, nn.Activations.RELU, defaultOutputActivation, defaultInputIds);
+        network = nn.generateNetwork(defaultNetworkShape, nn.Activations.RELU, defaultOutputActivation, defaultInputs);
         for (let i = 0; i < noSteps; i++) {
-            vis.step(network, trainingData, learningRate, defaultInputIds, defaultBatchSize);
+            vis.step(network, trainingData, learningRate, defaultInputs, defaultBatchSize);
         }
 
         for (let i = 0; i < trainingData.length; i++) {
             let datapoint = trainingData[i];
-            let prediction = nn.forwardPropagate(network, vis.constructInputs(datapoint.x1, datapoint.x2, defaultInputIds)) > 0 ? 1 : -1;
+            let prediction = nn.forwardPropagate(network, vis.constructInputs(datapoint.x1, datapoint.x2, defaultInputs)) > 0 ? 1 : -1;
             expect(prediction).toEqual(datapoint.y);
         }
 
