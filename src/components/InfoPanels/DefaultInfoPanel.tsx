a165e6260a74f6ca1d172434b81c257d4fa28082
Brandon Goodwin-Adeleke <bxg796@cs.bham.ac.uk>
2021-04-02 13:59:03 +0100

    Updated to do list and added list of conents for How To Get Started page

--- src/components/InfoPanels/DefaultInfoPanel.tsx
+++ src/components/InfoPanels/DefaultInfoPanel.tsx
@@ -6,6 +6,18 @@ function DefaultInfoPanel(props: NNConfig) {
     return (
         <div>
             <h1>How To Get Started</h1>
+            <ul>
+                <li>Add Home Button</li>
+                <li>Fix </li>
+                <li>Node representation</li>
+                <li>Link representation</li>
+                <li>Inputs</li>
+                <li>Data set</li>
+                <li>Building network</li>
+                <li>Training</li>
+                <li>Comparision func</li>
+                <li>Tool tips</li>
+            </ul>
             <p></p>
         </div>
     );
