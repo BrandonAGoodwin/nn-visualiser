e7f2fee4134603ec6edac866e00a1815e240142d
Brandon Goodwin-Adeleke <bxg796@cs.bham.ac.uk>
2021-04-14 18:46:51 +0100

    Cleaned up InfoPanelContext

--- src/contexts/InfoPanelContext.tsx
+++ src/contexts/InfoPanelContext.tsx
@@ -4,25 +4,31 @@ import DefaultInfoPanel from "../components/InfoPanels/DefaultInfoPanel";
 // If possble change any
 export const InfoPanelContext = createContext<any>({});
 
+const defaultInfoPanelData = {
+    infoPanel: <DefaultInfoPanel />,
+    infoPanelHistory: [],
+    infoPanelFuture: []
+};
+
+export interface InfoPanelData {
+    infoPanel: JSX.Element;
+    infoPanelHistory: JSX.Element[];
+    infoPanelFuture: JSX.Element[];
+}
+
 interface InfoPanelProps {
     children: React.ReactChild | React.ReactChild[];
 }
 
 export const InfoPanelProvider = (props: InfoPanelProps) => {
     const [infoPanelData, setInfoPanelData] = useState<InfoPanelData>(defaultInfoPanelData);
-    // const [infoPanel, setInfoPanel] = useState<JSX.Element>(<DefaultInfoPanel />);
-    // const [infoPanelHistory, setInfoPanelHistory] = useState<JSX.Element[]>([]);
-    // const [infoPanelFuture, setInfoPanelFuture] = useState<JSX.Element[]>([]);
+
 
     const setInfoPanelWrapper = (newInfoPanel: JSX.Element) => {
-        // console.log(newInfoPanel);
         setInfoPanelData((prevInfoPanelData) => {
             let { infoPanel, infoPanelHistory } = prevInfoPanelData;
             infoPanelHistory.push(infoPanel);
-            // console.log(newInfoPanelHistory);
-            // console.log(infoPanel)
 
-            // setInfoPanelHistory(newInfoPanelHistory);
             return { infoPanel: newInfoPanel, infoPanelHistory: infoPanelHistory, infoPanelFuture: [] };
         })
     }
@@ -34,9 +40,6 @@ export const InfoPanelProvider = (props: InfoPanelProps) => {
             if (infoPanelFuture.length !== 0) {
                 newInfoPanel = infoPanelFuture.shift();
                 infoPanelHistory.push(infoPanel);
-                // setInfoPanelHistory(newInfoPanelHistory);
-                // setInfoPanelFuture(newInfoPanelFuture);
-                // if (newPanel) setInfoPanel(newPanel);
             }
             if (newInfoPanel) {
                 return { infoPanel: newInfoPanel, infoPanelHistory: infoPanelHistory, infoPanelFuture: infoPanelFuture };
@@ -51,15 +54,8 @@ export const InfoPanelProvider = (props: InfoPanelProps) => {
             let { infoPanel, infoPanelHistory, infoPanelFuture } = prevInfoPanelData;
             let newInfoPanel: JSX.Element | undefined;
             if (infoPanelHistory.length !== 0) {
-                // let newInfoPanelHistory = infoPanelHistory;
-                // let newInfoPanelFuture = infoPanelFuture;
                 newInfoPanel = infoPanelHistory.pop();
-
                 infoPanelFuture.unshift(infoPanel);
-
-                // setInfoPanelHistory(newInfoPanelHistory);
-                // setInfoPanelFuture(newInfoPanelFuture);
-                // if (newPanel) setInfoPanel(newPanel);
             }
             if (newInfoPanel) {
                 return { infoPanel: newInfoPanel, infoPanelHistory: infoPanelHistory, infoPanelFuture: infoPanelFuture };
@@ -67,7 +63,6 @@ export const InfoPanelProvider = (props: InfoPanelProps) => {
                 return prevInfoPanelData;
             }
         })
-
     }
 
     const handleInfoPanelHome = () => {
@@ -90,63 +85,3 @@ export const InfoPanelProvider = (props: InfoPanelProps) => {
 
 
 
-const defaultInfoPanelData = {
-    infoPanel: <DefaultInfoPanel />,
-    infoPanelHistory: [],
-    infoPanelFuture: []
-};
-
-export interface InfoPanelData {
-    infoPanel: JSX.Element;
-    infoPanelHistory: JSX.Element[];
-    infoPanelFuture: JSX.Element[];
-}
-// export function useInfoPanel(defaultData: InfoPanelData = defaultInfoPanelData) {
-//     const [infoPanel, setInfoPanel] = useState<JSX.Element>(<DefaultInfoPanel />);
-//     const [infoPanelHistory, setInfoPanelHistory] = useState<JSX.Element[]>([]);
-//     const [infoPanelFuture, setInfoPanelFuture] = useState<JSX.Element[]>([]);
-
-
-//     const setInfoPanelWrapper = (newInfoPanel: JSX.Element) => {
-//         console.log(newInfoPanel);
-//         let newInfoPanelHistory = infoPanelHistory;
-//         newInfoPanelHistory.push(infoPanel);
-//         console.log(newInfoPanelHistory);
-//         console.log(infoPanel)
-//         setInfoPanel(newInfoPanel);
-//         setInfoPanelHistory(newInfoPanelHistory);
-//         setInfoPanelFuture([]);
-//     }
-
-//     const handleInfoPanelForward = () => {
-//         if (infoPanelFuture.length !== 0) {
-//             let newInfoPanelHistory = infoPanelHistory;
-//             let newInfoPanelFuture = infoPanelFuture;
-//             let newPanel = newInfoPanelFuture.shift();
-
-//             newInfoPanelHistory.push(infoPanel);
-
-//             setInfoPanelHistory(newInfoPanelHistory);
-//             setInfoPanelFuture(newInfoPanelFuture);
-//             if (newPanel) setInfoPanel(newPanel);
-//         }
-//     }
-
-//     const handleInfoPanelBackward = () => {
-//         if (infoPanelHistory.length !== 0) {
-//             let newInfoPanelHistory = infoPanelHistory;
-//             let newInfoPanelFuture = infoPanelFuture;
-//             let newPanel = newInfoPanelHistory.pop();
-
-//             newInfoPanelFuture.unshift(infoPanel);
-
-//             setInfoPanelHistory(newInfoPanelHistory);
-//             setInfoPanelFuture(newInfoPanelFuture);
-//             if (newPanel) setInfoPanel(newPanel);
-//         }
-//     }
-
-//     const handleInfoPanelHome = () => {
-//         setInfoPanelWrapper(<DefaultInfoPanel{...config} />);
-//     }
-// }
