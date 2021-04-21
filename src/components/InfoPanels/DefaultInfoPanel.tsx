ed1f077ca685bb7a9924727c0a7210f2abe7a388
Brandon Goodwin-Adeleke <bxg796@cs.bham.ac.uk>
2021-04-22 00:59:30 +0100

    Added to How To Get Started

--- src/components/InfoPanels/DefaultInfoPanel.tsx
+++ src/components/InfoPanels/DefaultInfoPanel.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import positiveLink from "../../PositiveLink.png";
 import negativeLink from "../../NegativeLink.png";
 import { ThemeContext } from "../../contexts/ThemeContext";
+import { Info } from "@material-ui/icons";
 
 function DefaultInfoPanel() {
 
@@ -10,7 +11,13 @@ function DefaultInfoPanel() {
     return (
         <div>
             <h1>How To Get Started</h1>
-            <p>This is a tool that lets you play with a simple neural network implementation. Below are instructions on how to get started designing your own neural network and test it's performance.</p>
+            <p>
+                This is a tool that lets you play with a simple neural network implementation. Below are instructions on how to get started designing your own neural network and test it's performance.
+                <ul>
+                    <li>There are some exercises to help show the effects of certain hyper-parameters that can be acces via the <b>info buttons</b> <Info fontSize={"small"} /> (e.g. batch size exercise and learning rate exercise).</li>
+                    <li>Use the Insights panel above to see properties of the current network configuration. </li>
+                </ul>
+           </p>
             <h2>Neural Network</h2>
             <h3>Links</h3>
             {/* Maths font w? */}
@@ -20,10 +27,10 @@ function DefaultInfoPanel() {
             <h4>Magnitude</h4>
             <p>The width of a link corresponds to the absolute magnitude of it's weight. Wider links have greater absolute weight values (and therefore have a stronger influence in the destination node) than narrower links.</p>
             <h4>Positive Value Link</h4>
-            <img src={positiveLink} alt="Positive link" width={100}/>
+            <img src={positiveLink} alt="Positive link" width={100} />
             <p>Links with a weight value &gt; 0 are {maxColourName}.</p>
             <h4>Negative Value Link</h4>
-            <img src={negativeLink} alt="Negative link" width={100}/>
+            <img src={negativeLink} alt="Negative link" width={100} />
             <p>Links with a weight value &lt; 0 are {minColourName}.</p>
             <h4>Mention/Link to:</h4>
             <ul>
